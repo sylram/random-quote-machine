82cfb598f8875f9c1a746f218ec8cb651dbbd2a2
Marlys Alvarado <sylram@icloud.com>
2024-04-24 17:27:26 -0400

    config file

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_UHx6HUrg2', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7svcf84dcubehv2tdt6cf82ve7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://master.dej043l7hxpv8.amplifyapp.com/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
