21e27468c11f99730a5b8c4b2fd617086cff6211
Marlys Alvarado <sylram@icloud.com>
2024-05-12 17:29:43 -0400

    favorite icon

--- src/CardComponent.js
+++ src/CardComponent.js
@@ -1,7 +1,9 @@
 import { render } from "@testing-library/react"
 import React from "react";
 import { Button } from 'react-bootstrap';
-import twitter from './twitter-x.svg'
+import twitter from './twitter-x.svg';
+import star from './star.svg';
+import starFill from './star-fill.svg'
 import { useState, useEffect } from "react";
 import { TwitterShareButton } from "react-share";
 
@@ -10,14 +12,15 @@ function CardComponent({ quotes }) {
 
   const [quote, setQuote] = useState('');
   const [autor, setAutor] = useState('');
-
-
+  const [active, setActive] = useState(false);
 
   const getRandomQuote = () => {
     let index = Math.ceil(Math.random() * (quotes.length));
     setQuote(quotes[index].quote);
     setAutor(quotes[index].author);
+    setActive(false);
     getRandomColor();
+    textTransition();
   }
 
   const getRandomColor = () => {
@@ -27,7 +30,18 @@ function CardComponent({ quotes }) {
     document.body.style.background = `rgb( ${red}, ${green}, ${blue} )`;
 
     document.getElementById('new-quote').style.background = `rgb( ${red}, ${green}, ${blue} )`;
+    document.getElementById('shareButton').style.background = `rgb( ${red}, ${green}, ${blue} )`;
+    document.getElementById('starButton').style.background = `rgb( ${red}, ${green}, ${blue} )`; 
   }
+
+  const toggleStar = () => {
+    setActive(!active);
+  }
+
+  const textTransition = () => {
+    document.getElementById('text').animate([{opacity:0},{opacity:1}],2000);
+  }
+
 
   useEffect(() => {
     let index = Math.ceil(Math.random() * (quotes.length));
@@ -38,27 +52,28 @@ function CardComponent({ quotes }) {
 
 
   return (
-
-  
-      <div id="quote-box" className="quote-box">
-        <div id="text" className="quote-text">
-          <span>{quote}</span>
-          </div>
-        <div id="author" className="quote-author">{autor}</div>
-        <div className="buttons">
-          <span id="link-container" className="link-container">
-          <TwitterShareButton url="https://random-quote-machine-fawn.vercel.app/" title="Check out this quote!"><img src={twitter} className="App-logo" alt="logo" /></TwitterShareButton>
-          </span>
-          <span id="button-container" className="button-container">
-            <button id="new-quote" type="button" className="btn btn-primary btn-sm button" onClick={getRandomQuote}>New Quote</button>
-          </span>
-
-        </div>
+    <div id="quote-box" className="quote-box">
+      <div id="text" className="quote-text">
+        <span>{quote}</span>
       </div>
-
+      <div id="author" className="quote-author">{autor}</div>
+      <div className="buttons">
+        <span id="link-container" className="link-container">
+          <TwitterShareButton id="shareButton" url="https://random-quote-machine-fawn.vercel.app/" className="btn btn-outline-secondary"
+           title="Check out this quote!">
+            <img src={twitter} className="twitterIcon"  alt="logo" /></TwitterShareButton>
+        </span>
+        <span className="starButton">
+            <button type="button" id="starButton" className="btn btn-outline-secondary" onClick={toggleStar}>
+              <img src={active ? starFill : star}></img>
+            </button>
+          </span>
+        <span id="button-container" className="button-container">  
+          <button id="new-quote" type="button" className="btn btn-primary btn-sm button" onClick={getRandomQuote}>New Quote</button>
+        </span>
+      </div>
+    </div>
   )
 }
 
 export default CardComponent;
-
-
